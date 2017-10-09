1c887e3d5e6c1b9a6202a4ac12b40d85b10e7288
lastmodified <32604595+lastmodified@users.noreply.github.com>
2017-10-09 20:55:53 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# kdramadl
+
+ Alternative downloader for [https://kdrama.anontpp.com](https://kdrama.anontpp.com) (via [/r/koreanvariety](https://www.reddit.com/r/koreanvariety/comments/723mtd/i_created_this_website_that_streams_korean_shows/?sort=new) or [/r/KDRAMA/](https://www.reddit.com/r/KDRAMA/comments/723n1y/i_created_this_website_that_streams_korean_shows/)).
+
+## Install
+
+Download the latest release for your OS.
+
+## Usage
+
+```
+NAME:
+   kdramadl - Make sure you have ffmpeg installed in PATH or in the current folder.
+
+USAGE:
+   kdramadl [global options] command [command options] [arguments...]
+
+VERSION:
+   0.0.1
+
+DESCRIPTION:
+   Alternative downloader for https://kdrama.anontpp.com
+
+COMMANDS:
+     help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --dc value        Download Code
+   --res value       Resolution of video. Choose from: "1080p" "720p" "480p" "360p". Default is "1080p".
+   --format value    Video format. Choose from: "mkv" "mp4". Default is "mkv".
+   --filename value  Filename to save as (without extension).
+   --sub             Download only subtitles.
+   --ffmpeg value    Path to ffmpeg executable. (default: "ffmpeg")
+   --folder value    Path to download folder.
+   --alt             Use kdrama.armsasuncion.com instead of kdrama.anontpp.com
+   --timeout value   Connection timeout interval in seconds. Default 10. (default: 10)
+   --help, -h        show help
+   --version, -v     print the version
+```
---
+++ kdramadl.go
@@ -0,0 +1,367 @@
+// Copyright (C) 2017 github.com/lastmodified
+//
+// This file is part of kdramadl.
+//
+// kdramadl is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// kdramadl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with kdramadl.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/urfave/cli"
+)
+
+const VERSION = "0.0.1"
+const FORMAT_MKV = "mkv"
+const FORMAT_MP4 = "mp4"
+
+var formats = []string{FORMAT_MKV, FORMAT_MP4}
+var resolutions = []string{"1080p", "720p", "480p", "360p"}
+
+var progHeader = fmt.Sprintf(
+	`=====================================================
+KDRAMA DOWNLOADER (%v)
+=====================================================
+`, VERSION)
+var userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/47.0 (Chrome)"
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func genFfmpegArgs(
+	ffmpegLogLevel string, timeout int,
+	vidUrl string, subUrl string,
+	format string, partFilePath string) []string {
+	args := []string{"-loglevel", ffmpegLogLevel, "-stats", "-y",
+		"-timeout", fmt.Sprintf("%v", timeout*1000000), // in microseconds
+		"-rw_timeout", fmt.Sprintf("%v", timeout*1000000), // in microseconds
+		"-reconnect", "1", "-reconnect_streamed", "1",
+		"-i", vidUrl, "-i", subUrl}
+	if format == FORMAT_MP4 {
+		args = append(
+			args, []string{"-c:s", "mov_text", "-c:v", "libx264", "-c:a", "copy"}...)
+	}
+	ffmpegOutputFormat := "mp4"
+	if format == FORMAT_MKV {
+		args = append(
+			args, []string{"-c", "copy"}...)
+		ffmpegOutputFormat = "matroska"
+	}
+	args = append(
+		args, []string{"-bsf:a", "aac_adtstoasc", "-f", ffmpegOutputFormat, partFilePath}...)
+	return args
+}
+
+func input(promptText string, reader *bufio.Reader) string {
+	fmt.Print(promptText)
+	response, _ := reader.ReadString('\n')
+	response = strings.Trim(response, " \n")
+	return response
+}
+
+func prnToScreen(text string) {
+	if strings.HasSuffix(text, "\n") {
+		fmt.Printf(text)
+	} else {
+		fmt.Println(text)
+	}
+}
+
+func prnInfo(text string) {
+	text = fmt.Sprintf("[i] %v", text)
+	prnToScreen(text)
+}
+
+func main() {
+
+	var (
+		dlCode     string
+		res        string
+		format     string
+		fileName   string
+		subOnly    bool
+		ffmpegPath string
+		dlFolder   string
+		altHost    bool
+		timeout    int
+	)
+	reader := bufio.NewReader(os.Stdin)
+
+	app := cli.NewApp()
+	app.Name = "kdramadl"
+	app.Version = VERSION
+	app.Copyright = "2017 https://github.com/lastmodified/"
+	app.Usage = "Alternative downloader for https://kdrama.anontpp.com"
+	app.Description = "Make sure you have ffmpeg installed in PATH or in the current folder."
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "c, code",
+			Usage:       "Download Code",
+			Destination: &dlCode,
+		},
+		cli.StringFlag{
+			Name: "r, resolution",
+			Usage: fmt.Sprintf(
+				"Resolution of video. Choose from: \"%v\". Default is \"%v\".",
+				strings.Join(resolutions, "\" \""),
+				resolutions[0]),
+			Destination: &res,
+		},
+		cli.StringFlag{
+			Name: "f, format",
+			Usage: fmt.Sprintf(
+				"Video format. Choose from: \"%v\". Default is \"%v\".",
+				strings.Join(formats, "\" \""),
+				formats[0]),
+			Destination: &format,
+		},
+		cli.StringFlag{
+			Name:        "filename",
+			Usage:       "Filename to save as (without extension).",
+			Destination: &fileName,
+		},
+		cli.BoolFlag{
+			Name:        "sub",
+			Usage:       "Download only subtitles.",
+			Destination: &subOnly,
+		},
+		cli.StringFlag{
+			Name:        "ffmpeg",
+			Value:       "ffmpeg",
+			Usage:       "Path to ffmpeg executable.",
+			Destination: &ffmpegPath,
+		},
+		cli.StringFlag{
+			Name:        "folder",
+			Value:       "",
+			Usage:       "Path to download folder.",
+			Destination: &dlFolder,
+		},
+		cli.BoolFlag{
+			Name:        "alt",
+			Usage:       "Use kdrama.armsasuncion.com instead of kdrama.anontpp.com",
+			Destination: &altHost,
+		},
+		cli.IntFlag{
+			Name:        "timeout",
+			Value:       10,
+			Usage:       "Connection timeout interval in seconds. Default 10.",
+			Destination: &timeout,
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+
+		fmt.Printf(progHeader)
+
+		if dlCode == "" {
+			dlCode = input("Enter the Download Code: ", reader)
+		}
+		if dlCode == "" {
+			return errors.New("Download Code cannot be blank")
+		}
+		if fileName == "" {
+			fileName = input("Enter the Filename (no extension): ", reader)
+		}
+		if fileName == "" {
+			return errors.New("Filename cannot be blank")
+		}
+		if res == "" {
+			res = input(fmt.Sprintf(
+				"Choose a Resolution (%v). Press ENTER to use the default (%v): ",
+				strings.Join(resolutions, ", "), resolutions[0]), reader)
+			if res == "" {
+				res = resolutions[0]
+			} else if stringInSlice(res, resolutions) != true {
+				return errors.New(fmt.Sprintf("Invalid resolution: %v", res))
+			}
+		}
+		if format == "" {
+			format = input(fmt.Sprintf(
+				"Choose a Format (%v). Press ENTER to use the default (%v): ",
+				strings.Join(formats, ", "), formats[0]), reader)
+			if format == "" {
+				format = formats[0]
+			} else if stringInSlice(format, formats) != true {
+				return errors.New(fmt.Sprintf("Invalid format: %v", format))
+			}
+		}
+
+		hostname := "https://kdrama.anontpp.com/"
+		if altHost == true {
+			hostname = "https://kdrama.armsasuncion.com/"
+		}
+		subUrl := fmt.Sprintf(
+			"%v?dcode=%v&downloadccsub=1", hostname, url.QueryEscape(dlCode))
+		vidUrl := fmt.Sprintf(
+			"%v?dcode=%v&quality=%v&downloadmp4vid=1", hostname,
+			url.QueryEscape(dlCode), url.QueryEscape(res))
+
+		var absFolderPath string
+		// cwd, _ := os.Getwd()
+
+		ex, _ := os.Executable()
+		cwd := filepath.Dir(ex)
+
+		if dlFolder != "" {
+			absFolderPath, _ = filepath.Abs(dlFolder)
+			if stat, err := os.Stat(absFolderPath); err != nil || !stat.IsDir() {
+				os.MkdirAll(dlFolder, os.ModePerm)
+				prnInfo(fmt.Sprintf("Created folder: %v", absFolderPath))
+			}
+		}
+		if absFolderPath == "" {
+			// default to current working folder
+			absFolderPath = cwd
+		}
+		subFilePath := path.Join(absFolderPath, fmt.Sprintf("%v.srt", fileName))
+		vidFilePath := path.Join(absFolderPath, fmt.Sprintf("%v.%v", fileName, format))
+		partFilePath := path.Join(absFolderPath, fmt.Sprintf("%v.%v.part", fileName, format))
+
+		fmt.Println(subFilePath)
+		fmt.Println(vidFilePath)
+		return nil
+		httpClient := &http.Client{}
+
+		// Download subtitles
+		if subOnly == true || format == FORMAT_MP4 {
+
+			request, _ := http.NewRequest("GET", subUrl, nil)
+			request.Header.Set("User-Agent", userAgent)
+			response, err := httpClient.Do(request)
+			if err != nil {
+				return errors.New(
+					fmt.Sprintf("Error downloading subtitles: %v", err))
+			}
+			if response.StatusCode >= 400 {
+				return errors.New(
+					fmt.Sprintf("Error downloading subtitles: HTTP %v", response.StatusCode))
+			}
+			contentType := response.Header.Get("content-type")
+			if strings.Contains(contentType, "text/html") {
+				return errors.New(
+					fmt.Sprintf(
+						"Error downloading subtitles: Unexpected Content-Type \"%v\"",
+						contentType))
+			}
+			defer response.Body.Close()
+			output, err := os.Create(subFilePath)
+			if err != nil {
+				return errors.New(fmt.Sprintf("%v already exists", subFilePath))
+			}
+			defer output.Close()
+			if _, err := io.Copy(output, response.Body); err != nil {
+				return errors.New(
+					fmt.Sprintf("Error downloading subtitles: %v", err))
+			}
+			prnInfo(fmt.Sprintf("Saved subtitles: %v", subFilePath))
+		}
+		if subOnly == true {
+			return nil
+		}
+
+		// List of potential ffmpeg paths
+		ffmpegPaths := []string{
+			ffmpegPath, path.Join(cwd, "ffmpeg"), path.Join(cwd, "ffmpeg.exe")}
+
+		var verifiedFfmpegPath string
+		for i := 0; i < len(ffmpegPaths); i++ {
+			ffmpegCmd := exec.Command(ffmpegPaths[i], "-version")
+			err := ffmpegCmd.Run()
+			if err == nil {
+				verifiedFfmpegPath = ffmpegPaths[i]
+				break
+			}
+		}
+		if verifiedFfmpegPath == "" {
+			// no ffmpeg found
+			return errors.New("Unable to find valid ffmpeg path")
+		}
+		ffmpegLogLevel := "fatal"
+		args := genFfmpegArgs(
+			ffmpegLogLevel, timeout, vidUrl, subUrl, format, partFilePath)
+		ffmpegCmd := exec.Command(verifiedFfmpegPath, args...)
+		ffmpegCmd.Stderr = os.Stderr
+		ffmpegCmd.Stdout = os.Stdout
+		ffmpegCmd.Stdin = os.Stdin
+
+		if err := ffmpegCmd.Run(); err != nil {
+			// Retry with a more verbose loglevel
+			args := genFfmpegArgs(
+				"warning", timeout, vidUrl, subUrl, format, partFilePath)
+			ffmpegCmd := exec.Command(verifiedFfmpegPath, args...)
+			ffmpegCmd.Stderr = os.Stderr
+			ffmpegCmd.Stdout = os.Stdout
+			ffmpegCmd.Stdin = os.Stdin
+			err := ffmpegCmd.Run()
+			if err != nil {
+				// Do http request to check what's wrong
+				request, _ := http.NewRequest("GET", subUrl, nil)
+				request.Header.Set("User-Agent", userAgent)
+				response, err := httpClient.Do(request)
+
+				if err != nil {
+					return errors.New(
+						fmt.Sprintf("Error downloading video: %v", err))
+				}
+				if response.StatusCode >= 400 {
+					return errors.New(
+						fmt.Sprintf("Error downloading video: HTTP %v", response.StatusCode))
+				}
+				contentType := response.Header.Get("content-type")
+				if strings.Contains(contentType, "text/html") {
+					return errors.New(
+						fmt.Sprintf(
+							"Error downloading video: Unexpected Content-Type \"%v\"",
+							contentType))
+				}
+				return errors.New(fmt.Sprintf("ffmpeg Error: %v", err))
+			}
+		}
+		if _, err := os.Stat(partFilePath); !os.IsNotExist(err) {
+			// rename .part file to final filename
+			err := os.Rename(partFilePath, vidFilePath)
+			if err != nil {
+				fmt.Printf("Unable to rename file %v to %v\n", partFilePath, vidFilePath)
+				return errors.New("Unable to rename file")
+			}
+		}
+		if _, err := os.Stat(vidFilePath); !os.IsNotExist(err) {
+			prnInfo(fmt.Sprintf("Saved video %v", vidFilePath))
+		}
+		return nil
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("\n[x] %v\n\n", err)
+	}
+	input("\bPress ENTER to continue...", reader)
+}
